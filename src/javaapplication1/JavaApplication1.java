50c306ebfb6a04e8a6a0ec5fc32d01422b3ab658
admin <admin@Ramanan>
2013-03-19 12:00:56 +0530

    Test

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author admin
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Printing");
+    }
+}
